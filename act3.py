62cba19d1499ad27e5aa7652c5f7a5df702e5897
sharan-0222 <sharanmenonc@gmail.com>
2025-03-20 19:57:44 +0530

    Add files via upload

---
+++ act3.py
@@ -0,0 +1,10 @@
+valid=False
+while not valid :
+        try:
+         n=int(input("please enter a number"))
+         while n%2==0:
+                 print("bye")
+        except:
+              print("invalid")
+                     
+        
